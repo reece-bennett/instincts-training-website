f31838806d4393f150ba382ba1b6939579e038f2
Reece Bennett <reece.bennett1@gmail.com>
2022-11-28 18:37:34 +0000

    Configure Tailwind CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-const { dest, parallel, series, src } = require('gulp');
+const { dest, parallel, series, src, watch } = require('gulp');
+const browserSync = require('browser-sync');
 const del = require('del');
 const hb = require('gulp-hb');
+const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 
+/*
+* Build steps
+*/
 function clean() {
   return del('build/');
 }
@@ -14,13 +20,51 @@ function html() {
     .pipe(dest('build/'));
 }
 
-function assets() {
-  return src('assets/**/*')
+function css() {
+  return src('src/css/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/'));
 }
 
+function assets() {
+  return src('src/assets/**/*')
+    .pipe(dest('build/'));
+}
+
+/*
+* Browser-Sync
+*/
+const server = browserSync.create();
+
+function bsServe(done) {
+  server.init({
+    server: {
+      baseDir: 'build/'
+    },
+    open: false
+  });
+  done();
+}
+
+function bsReload(done) {
+  server.reload();
+  done();
+}
+
+function bsWatch() {
+  watch('src/assets/**/*', series(assets, bsReload));
+  watch(['src/pages/**/*.hbs', 'src/partials/**/*.hbs'], series(html, bsReload));
+  watch('src/css/**/*.css', css);
+}
+
+/*
+* Expose tasks to gulp CLI
+*/
 exports.assets = assets;
 exports.clean = clean;
+exports.css = css;
 exports.html = html;
-exports.build = series(clean, parallel(html, assets));
-exports.default = exports.build;
+exports.build = series(clean, parallel(html, css, assets));
+exports.default = series(exports.build, bsServe, bsWatch);
