59197419cc22b4380ae4fdb4f8e200e9ffe6f1f8
Reece Bennett <reece.bennett1@gmail.com>
2023-06-04 15:54:23 +0100

    Make accordion content show when no JS

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,6 +1,7 @@
 const accordionHeaders = document.querySelectorAll('[data-module="accordion"]');
 accordionHeaders.forEach(accordionHeader => {
   let target = accordionHeader.parentElement.nextElementSibling;
+  target.hidden = true
   accordionHeader.onclick = () => {
     let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
     accordionHeader.setAttribute('aria-expanded', !expanded);
